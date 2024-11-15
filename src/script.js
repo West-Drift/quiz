8566cda8f3620460c7b3098225195ed288ab69c2
West-Drift <120048072+West-Drift@users.noreply.github.com>
2024-11-15 09:18:41 +0300

    changes

--- src/script.js
+++ src/script.js
@@ -1,47 +1,163 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const cards = document.querySelectorAll(".card");
-  let selectedCards = [];
-  let matchedCards = [];
+const startBtn = document.querySelector(".start-btn");
+const popupInfo = document.querySelector(".popup-info");
+const exitBtn = document.querySelector(".exit-btn");
+const main = document.querySelector(".main");
+const continueBtn = document.querySelector(".continue-btn");
+const quizSection = document.querySelector(".quiz-section");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
+const tryAgainBtn = document.querySelector(".tryAgain-btn");
+const goHomeBtn = document.querySelector(".goHome-btn");
 
-  cards.forEach((card) => {
-    card.addEventListener("click", () => {
-      const column = card.closest(".column").dataset.column;
+startBtn.onclick = () => {
+  popupInfo.classList.add("active");
+  main.classList.add("active");
+};
 
-      if (
-        selectedCards.length < 2 &&
-        !card.classList.contains("matched") &&
-        !card.classList.contains("inactive")
-      ) {
-        if (
-          selectedCards.length === 1 &&
-          selectedCards[0].closest(".column").dataset.column === column
-        ) {
-          selectedCards[0].classList.remove("selected");
-          selectedCards = [];
-        }
+exitBtn.onclick = () => {
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+};
 
-        card.classList.add("selected");
-        selectedCards.push(card);
+continueBtn.onclick = () => {
+  popupInfo.classList.remove("active");
+};
 
-        if (selectedCards.length === 2) {
-          setTimeout(() => {
-            const [firstCard, secondCard] = selectedCards;
-            if (firstCard.dataset.id === secondCard.dataset.id) {
-              firstCard.classList.add("matched");
-              secondCard.classList.add("matched");
-              matchedCards.push(firstCard, secondCard);
-              firstCard.classList.add("correct");
-              secondCard.classList.add("correct");
-            } else {
-              firstCard.classList.add("incorrect");
-              secondCard.classList.add("incorrect");
-            }
-            firstCard.classList.add("inactive");
-            secondCard.classList.add("inactive");
-            selectedCards = [];
-          }, 1000);
-        }
+continueBtn.onclick = () => {
+  quizSection.classList.add("active");
+  popupInfo.classList.remove("active");
+  main.classList.remove("active");
+  quizBox.classList.add("active");
+
+  showQuestions(0);
+  questionCounter(1);
+  headerScore();
+};
+
+tryAgainBtn.onclick = () => {
+  quizBox.classList.add("active");
+  nextBtn.classList.remove("active");
+  resultBox.classList.remove("active");
+
+  questionCount = 0;
+  questionNumb = 1;
+  userScore = 0;
+  showQuestions(questionCount);
+  questionCounter(questionNumb);
+
+  headerScore();
+};
+
+goHomeBtn.onclick = () => {
+  quizSection.classList.remove("active");
+  nextBtn.classList.remove("active");
+  resultBox.classList.remove("active");
+
+  questionCount = 0;
+  questionNumb = 1;
+  userScore = 0;
+  showQuestions(questionCount);
+  questionCounter(questionNumb);
+};
+
+let questionCount = 0;
+let questionNumb = 1;
+let userScore = 0;
+
+const nextBtn = document.querySelector(".next-btn");
+
+nextBtn.onclick = () => {
+  if (questionCount < questions.length - 1) {
+    questionCount++;
+    showQuestions(questionCount);
+
+    questionNumb++;
+    questionCounter(questionNumb);
+
+    nextBtn.classList.remove("active");
+  } else {
+    showResultBox();
+  }
+};
+
+const optionList = document.querySelector(".option-list");
+
+// getting questions and options from array
+function showQuestions(index) {
+  const questionText = document.querySelector(".question-text");
+  questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+  let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+  optionList.innerHTML = optionTag;
+
+  const option = document.querySelectorAll(".option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+function optionSelected(answer) {
+  let userAnswer = answer.textContent;
+  let correctAnswer = questions[questionCount].answer;
+  let allOptions = optionList.children.length;
+
+  if (userAnswer == correctAnswer) {
+    answer.classList.add("correct");
+    userScore += 1;
+    headerScore();
+  } else {
+    answer.classList.add("incorrect");
+    // if answer incorrect, auto select correct answer
+    for (let i = 0; i < allOptions; i++) {
+      if (optionList.children[i].textContent == correctAnswer) {
+        optionList.children[i].classList.add("correct");
       }
-    });
-  });
-});
+    }
+  }
+  // if user has selected, disable all options
+  for (let i = 0; i < allOptions; i++) {
+    optionList.children[i].classList.add("disabled");
+  }
+  nextBtn.classList.add("active");
+}
+
+function questionCounter(index) {
+  const questionTotal = document.querySelector(".question-total");
+  questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+function headerScore() {
+  const headerScoreText = document.querySelector(".header-score");
+  headerScoreText.textContent = `Score: ${userScore} / ${questions.length}`;
+}
+
+function showResultBox() {
+  quizBox.classList.remove("active");
+  resultBox.classList.add("active");
+
+  const scoreText = document.querySelector(".score-text");
+  scoreText.textContent = `Your Score ${userScore} out of ${questions.length}`;
+
+  const circularProgress = document.querySelector(".circular-progress");
+  const progressValue = document.querySelector(".progress-value");
+  let progressStartValue = 0;
+  let progressEndValue = (userScore / questions.length) * 100;
+  let speed = 20;
+
+  let progress = setInterval(() => {
+    progressStartValue++;
+
+    progressValue.textContent = `${progressStartValue}%`;
+    circularProgress.style.background = `conic-gradient(#c40094 ${
+      progressStartValue * 3.6
+    }deg, rgba(255, 255, 255, .1) 0deg)`;
+
+    if (progressStartValue == progressEndValue) {
+      clearInterval(progress);
+    }
+  }, speed);
+}
